3c694bd9e55c247cd7848bd13686e75681192aaa
Alessio Biancalana <dottorblaster@gmail.com>
2020-12-29 19:27:21 +0100

    Replace test case with a proper Jest test suite

---
+++ hoverlord.test.js
@@ -0,0 +1,28 @@
+const { spawn, call, send, shutdown } = require('./index');
+
+describe('hoverlord', () => {
+  it('inspects the state of a process', async () => {
+    const actor = await spawn(() => {
+      const { receive, reply } = require('./index');
+      return receive((state, message) => {
+        switch (message.message) {
+          case 'ping':
+            return state + 1;
+          case 'pang':
+            reply(message, state);
+            return state;
+          default:
+            return state;
+        }
+      }, 0);
+    }, 'demoActor');
+    send('demoActor', 'ping');
+    send('demoActor', 'ping');
+    send('demoActor', 'ping');
+    send('demoActor', 'ping');
+
+    const { message: result } = await call('demoActor', 'pang');
+    shutdown();
+    expect(result).toBe(4); 
+  });
+});
