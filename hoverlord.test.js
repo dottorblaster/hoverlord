a92a01da083bd55fa5c5f807902b614ded28970f
Alessio Biancalana <dottorblaster@gmail.com>
2020-12-30 17:27:07 +0100

    Add a new Jest test to check process to process communication

--- hoverlord.test.js
+++ hoverlord.test.js
@@ -1,7 +1,9 @@
 const { spawn, call, send, shutdown } = require('./index');
 
+jest.setTimeout(30000);
+
 describe('hoverlord', () => {
-  it('inspects the state of a process', async () => {
+  it('can call from the main process', async () => {
     await spawn(() => {
       const { receive, reply } = require('./index');
       return receive((state, message) => {
@@ -24,5 +26,56 @@ describe('hoverlord', () => {
     const { message: result } = await call('demoActor', 'pang');
     shutdown();
     expect(result).toBe(4);
+  });
+
+  it('can call a process from another process', async () => {
+    await spawn(() => {
+      const { receive, reply } = require('./index');
+      return receive(
+        (state, message) => {
+          if (
+            Object.prototype.hasOwnProperty.call(message.message, 'fakeCount')
+          ) {
+            reply(message, state);
+            return message.message;
+          }
+          if (message.message === 'inspect') {
+            reply(message, state);
+          }
+          return state;
+        },
+        [1, 2, 3],
+      );
+    }, 'statefulActor');
+
+    await spawn(() => {
+      const { receive, reply, call } = require('./index');
+      return receive((state, message) => {
+        switch (message.message) {
+          case 'do_the_call': {
+            const newState = { fakeCount: 1000 };
+            call('statefulActor', newState).then((remoteState) => {
+              reply(message, ['done', remoteState]);
+            });
+            return null;
+          }
+          default:
+            return state;
+        }
+      });
+    }, 'caller');
+
+    const {
+      message: [callerResponse, remoteState],
+    } = await call('caller', 'do_the_call');
+
+    const { message: result } = await call('statefulActor', 'inspect');
+
+    shutdown();
+
+    expect(callerResponse).toBe('done');
+    expect(remoteState.message).toEqual([1, 2, 3]);
+    expect(remoteState.fromWorker).toBe(true);
+    expect(result).toEqual({ fakeCount: 1000 });
   });
 });
